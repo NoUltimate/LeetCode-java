bf2dde4ee66660e8182b56f8acd98bb7be39698a
bowensun <bowensun@whale.im>
2021-07-16 17:31:02 +0800

    temp save

--- src/com/java/com/leetcode/offer/hw/Solution.java
+++ src/com/java/com/leetcode/offer/hw/Solution.java
@@ -13,20 +13,17 @@ import java.util.*;
 
 public class Solution {
 
-    public boolean canJump(int[] nums) {
-        // 从位置i开始能到达的最远位置
-        if (nums.length == 0) {
-            return false;
-        }
-        int energy = nums[0];
-        int i = 1;
-        while (energy != 0 && i < nums.length) {
-            energy--;
-            if (energy < nums[i]) {
-                energy = nums[i];
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // 从i到j之间的最大值
+        int[][] dp = new int[nums.length][nums.length];
+        for(int i = 0; i < nums.length; ++i) {
+            for(int j = i; j < nums.length; ++j) {
+                if (i == j) {
+                    dp[i][j] = nums[i];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], nums[j]);
+                }
             }
-            ++i;
         }
-        return i == nums.length;
     }
 }
