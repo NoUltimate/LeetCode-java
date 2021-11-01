027643ff9f1b305fb4e47d55b3aa26d0b9dc11b2
bowensun <bowensun@whale.im>
2021-11-01 10:18:35 +0800

    update

--- src/main/java/leetcode/Solution.java
+++ src/main/java/leetcode/Solution.java
@@ -6,13 +6,31 @@ package leetcode;
  * @date 2021年07月20日 16:32
  */
 
-import util.ListNode;
-
 import java.util.*;
 
-public class Solution {
-    public int minSwaps(String s) {
+class Solution {
 
+    public int[][] findContinuousSequence(int target) {
+        int i = 1, j = 1, sum = 1;
+        List<int[]> list = new ArrayList<>();
+        while (true) {
+            if (j - i + 1 == 2 && i + j > target) break;
+            if (sum < target) {
+                j++;
+                sum += j;
+            } else if (sum > target) {
+                sum -= i;
+                i++;
+            } else {
+                int[] temp = new int[j - i + 1];
+                int index = 0;
+                for(int k = i; k <= j; ++k) temp[index++] = k;
+                list.add(temp);
+            }
+        }
+        int[][] result = new int[list.size()][];
+        for(int k = 0; k < list.size(); ++k) result[k] = list.get(k);
+        return result;
     }
-}
 
+}
